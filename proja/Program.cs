6209e25dc39ee9750aa7d0f29df54afaa7286e29
usern4mehere9 <91462628+usern4mehere9@users.noreply.github.com>
2021-10-10 15:25:19 +0200

    Add files via upload

---
+++ proja/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace proja
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double kb, pb, kbp, v, aux;
+
+            kb = double.Parse(Console.ReadLine());
+            pb = double.Parse(Console.ReadLine());
+
+            kbp = kb / 2;
+
+            if (kbp < pb)
+            {
+                v = (kbp + kb) / 2;
+                Console.WriteLine(Math.Round(v, 2));
+            }
+
+            else
+            {
+                aux = 2 * pb;
+                v = (aux + pb) / 2;
+                Console.WriteLine(Math.Round(v, 2));
+            }
+        }
+    }
+}
